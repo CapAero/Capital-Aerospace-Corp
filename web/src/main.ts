06e36874df3fb20c74c8d10b82ccc14d2f503758
Michael Johnson <michael@icefoganalytics.com>
2025-10-07 15:35:23 -0700

    Google Analytics

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,11 +5,21 @@ import vuetify from "@/plugins/vuetify-plugin"
 
 import VueScrollTo from "vue-scrollto"
 import router from "@/router"
+import { createGtag } from "vue-gtag"
 
 import App from "@/App.vue"
 
 const app = createApp(App)
 app.use(router).use(vuetify)
+
+app.use(
+  createGtag({
+    tagId: "G-P51CQHC2M7",
+    pageTracker: {
+      router,
+    },
+  })
+)
 
 app.mount("#app")
 
