b75d344790aed01516a541ee8ba925ec1c6aeae7
Kevin Beamish <k.beamish@capaerocorp.com>
2025-10-07 17:10:39 -0700

    Bio change

--- web/src/pages/TeamPage.vue
+++ web/src/pages/TeamPage.vue
@@ -29,13 +29,7 @@
           subtitle="President/PRM"
           icon="/images/staff/KevinBeamish.jpg"
         >
-          Kevin has over 25 years of experience in the aviation industry, including 15 years in
-          management roles. He has held positions such as Director of Maintenance and Director of
-          Operations at various Canadian charter operators. Kevin is a licensed Aircraft Maintenance
-          Engineer (AME) with a specialty in avionics and holds a Bachelor of Commerce degree from
-          the University of Calgary. He is also a Transport Canada Designated Person for Continuing
-          Airworthiness Management Organizations (CAMO) and has served as a member of the Canadian
-          Business Aviation Association (CBAA) Safety Committee.
+          Kevin's Bio Coming Soon!
         </StaffCard>
 
         <StaffCard
@@ -43,11 +37,7 @@
           subtitle="Team Lead & AME"
           icon="/images/staff/Trevor.jpg"
         >
-          Our Maintenance Planning and Logistics services ensure precise coordination between the
-          maintenance and operations departments. Your operation will have a dedicated Capital
-          Aerospace staff member whose primary focus is supporting your operational needs. This
-          structure allows us to adapt seamlessly to real time changes and evolving operational
-          demands.
+          Trevor's Bio Coming Soon!
         </StaffCard>
 
         <StaffCard
@@ -55,93 +45,56 @@
           subtitle="Technology and Innovation Lead & AME"
           icon="/images/staff/KyleLandry.jpg"
         >
-          Our Inventory Control program provides accurate tracking and management of your aircraft
-          parts, following the same proven model used by established in-house AMOs. We will maintain
-          or help you build a reliable base inventory, manage ordering and forecasting, oversee
-          shipping and receiving, conduct audits, and provide financial analysis. By leveraging our
-          supplier relationships and discounts, we ensure you receive the best pricing available
-          while maintaining the highest standards of accountability and efficiency.
+         Kyle's Bio Coming Soon!
         </StaffCard>
         <StaffCard
           title="Jay Chow"
           subtitle="Technical Records and Stores Manager"
           icon="/images/staff/JayChow.jpg"
         >
-          Our Operations Staff Training program provides structured annual and progressive training
-          to strengthen coordination between operations and maintenance departments. Each program is
-          customized to your fleet type and operational procedures, ensuring pilots and
-          administrative personnel gain a clear understanding of maintenance requirements and
-          compliance standards. Training includes onboarding and practical application of our
-          custom-built maintenance tracking technology, enabling staff to accurately manage records,
-          monitor aircraft status, and respond effectively to operational changes.
+          Jay's Bio Coming Soon!
         </StaffCard>
         <StaffCard
           title="Mark Cunningham"
           subtitle="AME"
           icon="/images/staff/stock.png"
         >
-          We can either support or assume full responsibility for the PRMC duties associated with
-          your AOC. Your Maintenance Control Manual will be reviewed, maintained, and kept current
-          to regulatory standards. Our team can act as a guiding resource during internal and
-          external audits, or manage all aspects of maintenance control and technical dispatch on
-          your behalf. Our goal is to carry as much of the Transport Canada compliance burden as
-          possible, allowing you to focus on safe and efficient operations.
+          Mark's Bio Coming Soon!
         </StaffCard>
         <StaffCard
           title="Owen Davis"
           subtitle="AME"
           icon="/images/staff/OwenDavis.jpg"
         >
-          This service provides the option of building or replacing the Documents Incorporated for
-          Reference manual to ensure it aligns with current regulatory and operational requirements.
-          We manage CHRs and technical records for every aircraft in your fleet, updating all
-          records with accuracy and consistency. Our team also oversees the precision and
-          effectiveness of aircraft journey logbooks, working directly with your pilots to maintain
-          accuracy and ensure full regulatory compliance.
+          Owen's Bio Coming Soon!
         </StaffCard>
         <StaffCard
           title="Damon Juss"
           subtitle="AME & Turbine Specialist"
           icon="/images/staff/stock.png"
         >
-          This is a comprehensive service designed to help you reliably accept new aircraft into
-          your fleet or operation. We communicate directly with aircraft owners and leasing
-          companies to verify aircraft condition, fitness, and conformity with your brand and
-          operational standards. Our team provides guidance and financial analysis throughout the
-          purchasing process, while managing the detailed tasks of initial inspections, including
-          aircraft assessments and independent appraisals.
+         Damon's Bio Coming Soon!
         </StaffCard>
         <StaffCard
           title="Jesse Kirby"
           subtitle="Apprentice & Turbine Specialist"
           icon="/images/staff/JesseKirby.jpg"
         >
-          We work directly with your management staff to determine the most suitable aircraft for
-          both current operations and future fleet growth. By analyzing the maintenance constraints
-          and advantages of specific aircraft types, we provide clear guidance for long-term
-          planning. This can include preparing for infrastructure expansion and supporting regional
-          growth strategies. With our expertise, aircraft maintenance is no longer a limiting
-          factor, allowing your organization to focus fully on achieving its goals.
+         Jesse's Bio Coming Soon!
         </StaffCard>
         <StaffCard
           title="Ryan Purcell"
           subtitle="Apprentice"
           icon="/images/staff/RyanPurcell.jpg"
         >
-          Our Health and Safety Program is designed to integrate seamlessly with your operation,
-          ensuring full compliance with regulatory requirements while fostering a proactive safety
-          culture. We support client obligations and can scale a company-wide safety program to meet
-          your specific needs, enabling you to concentrate on flight operations and safety.
+          Ryan's Bio Coming Soon!
         </StaffCard>
         <StaffCard
           title="Andrew Sinding"
           subtitle="Apprentice"
           icon="/images/staff/AndrewSinding.jpg"
         >
-          Our Health and Safety Program is designed to integrate seamlessly with your operation,
-          ensuring full compliance with regulatory requirements while fostering a proactive safety
-          culture. We support client obligations and can scale a company-wide safety program to meet
-          your specific needs, enabling you to concentrate on flight operations and safety.
+          Andrew's Bio Coming Soon!
         </StaffCard>
       </v-row>
     </div>
